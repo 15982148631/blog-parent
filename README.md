65b1672c08b1b9d9f8e1c5cc36071dbed5f0fbcd
wyf <996477396@qq.com>
2020-11-27 23:02:19 +0800

    15.新增编译脚本，项目检查完成进度

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 ### 2. blog-apiserver-dashboard
 测试访问：http://localhost:9101/api/dashboard/core/admin/get/1
+
+## 注意
+1. 为了便于操作增加了call.bat脚本进行jar编译打包
+
---
+++ bin/call.bat
@@ -0,0 +1,29 @@
+cd ..
+cd blog-dependencies
+call mvn clean install
+
+cd ..
+cd blog-deparent
+call mvn clean install
+
+cd ..
+cd blog-common
+call mvn clean install
+
+cd ..
+cd blog-repository
+call mvn clean install
+
+cd ..
+cd blog-business
+call mvn clean install
+
+cd ..
+cd blog-controller
+call mvn clean install
+
+cd ..
+cd blog-all
+call mvn clean install
+
+cd ..
